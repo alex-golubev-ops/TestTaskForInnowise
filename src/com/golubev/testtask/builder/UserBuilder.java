5f8fd8b313c6df88f3f7fb4f86b8d79e5d60fb87
AlexGolubev <golubevaleksej7@gmail.com>
2021-02-08 19:40:48 +0300

    Create interface UserBuilder

---
+++ src/com/golubev/testtask/builder/UserBuilder.java
@@ -0,0 +1,9 @@
+package com.golubev.testtask.builder;
+
+import com.golubev.testtask.entity.Role;
+import com.golubev.testtask.entity.User;
+import java.util.List;
+
+public interface UserBuilder {
+    User buildUser();
+}
