eff97e591b85558e0c7fa45165ee9ff6608c4d9a
AlexGolubev <golubevaleksej7@gmail.com>
2021-02-10 14:26:28 +0300

    Update Role: Add getRole method

---
+++ src/java/com/golubev/testtask/entity/Role.java
@@ -0,0 +1,49 @@
+package com.golubev.testtask.entity;
+
+import com.golubev.testtask.exception.RoleException;
+
+public enum Role {
+    USER(1),
+    CUSTOMER(1),
+    ADMIN(2),
+    PROVIDER(2),
+    SUPER_ADMIN(3);
+
+    private final int level;
+
+    Role(int level){
+        this.level=level;
+    }
+
+    public static Role getRole(int number){
+        switch (number){
+            case 1:{
+
+                return Role.USER;
+
+            }
+            case 2:{
+
+                return Role.CUSTOMER;
+
+            }
+            case 3:{
+
+                return Role.ADMIN;
+
+            }
+            case 4:{
+
+                return Role.PROVIDER;
+
+            }
+            case 5:{
+
+                return Role.SUPER_ADMIN;
+
+
+            }
+            default: throw new RoleException("Not found role type with number " + number);
+        }
+    }
+}
